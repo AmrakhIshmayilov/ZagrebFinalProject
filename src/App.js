fd87d21acbd4998d74a03c6c44df060c9036bf45
xayala86 <xayalaaslan86@gmail.com>
2023-06-21 15:01:28 +0400

    Navbar is completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import "./App.css";
 import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <h1> test </h1>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
